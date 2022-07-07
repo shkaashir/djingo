99697fde68718f28494b3948801c00de0355fc48
Prabin Pandey <prabin.pandey@ekbana.info>
2022-07-07 16:02:28 +0545

    changed ui of bingo and add color to bino number div

--- static/js/main.js
+++ static/js/main.js
@@ -26,23 +26,38 @@ function onClose(event) {
 
 function onMessage(event) {
    const data = JSON.parse(event.data)
-   addDataToTable(data)
+   console.log(data)
+   if(Array.isArray(data)){
+    addDataToTable(data)
+   }else{
+    addRandomGeneratedNumberToElement(data.roulette_number)
+   }
+}
+
+function addRandomGeneratedNumberToElement(number){
+    document.getElementById('random-number').innerText=number
 }
 
 function addDataToTable(data){
+    let  i = 0
+    const color = ['#B30E50','#FF3300','#FC4B54','#FD882D','#43C2C1']
     document.getElementById('bingo-table-body').innerHTML=''
-    for (let i=0;i<data.length;i++){
+    while (i<data.length){
         let tr = document.createElement("tr")
-        // document.getElementById('bingo-table-body').innerHTML+=tr
-        for(let j=0;j<data.length;j++){
+        for (let j=0;j<data.length;j++){
+            let res = data[j][i]
+            if (i===2 && i===j){
+                res = 'free'
+            }
             tr.innerHTML+=`
                 <td>
-                    <div class="bingo-circ position-relative">
-                        <p class="position-absolute top-50 translate-middle">${data[i][j]}</p>
+                    <div class="bingo-circ position-relative" style="background-color:${color[j]}">
+                        <p class="position-absolute top-50 translate-middle">${res}</p>
                     </div>
                 </td>
             `
         }
         document.getElementById('bingo-table-body').appendChild(tr);
+        i+=1
     }
 }
